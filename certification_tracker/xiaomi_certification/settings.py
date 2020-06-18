4d9808345869504ec12c3f75788462ce4eddcd94
yshalsager <ysh-alsager@hotmail.com>
2020-06-18 17:48:34 +0200

    mi_global: Initial spider

--- certification_tracker/xiaomi_certification/settings.py
+++ certification_tracker/xiaomi_certification/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'xiaomi_certification.spiders'
 #USER_AGENT = 'xiaomi_certification (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- certification_tracker/xiaomi_certification/spiders/mi_global.py
+++ certification_tracker/xiaomi_certification/spiders/mi_global.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy import Spider
 
 
-class MiGlobalSpider(scrapy.Spider):
+class MiGlobalSpider(Spider):
     name = 'mi_global'
-    allowed_domains = ['https://www.mi.com/global/certification/compliance/']
-    start_urls = ['http://https://www.mi.com/global/certification/compliance//']
+    allowed_domains = ['mi.com']
+    start_urls = ['https://www.mi.com/global/certification/compliance/',
+                  'https://www.mi.com/in/certification/compliance/']
 
     def parse(self, response):
-        pass
+        items = response.xpath('//div[@class="item"]/ul/li')
+        for item in items:
+            yield {
+                'device': item.xpath('.//a/text()').get(),
+                'certificate': item.xpath('.//a/@href').get()
+            }
