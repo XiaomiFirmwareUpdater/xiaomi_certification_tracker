9438a99522f238586ce2dbf95a7b3aa67acf6c6b
yshalsager <ysh-alsager@hotmail.com>
2020-06-19 19:52:14 +0200

    Implement MIIT scraper

---
+++ certification_tracker/xiaomi_certification/database/tables/miit.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Table, Column, Integer, String
+
+from certification_tracker.xiaomi_certification.database import metadata
+
+
+def create_table(table):
+      Table(table, metadata,
+            Column('id', Integer(), primary_key=True, autoincrement=True),
+            Column('device', String(), nullable=False),
+            Column('model', String(), nullable=False),
+            Column('category', String(), nullable=False),
+            Column('date', String()),
+            Column('certification', String(), nullable=False)
+            )
