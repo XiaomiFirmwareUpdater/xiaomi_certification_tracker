687c698c5f38787d9770dd6196c290840d9d3056
yshalsager <ysh-alsager@hotmail.com>
2022-09-26 16:24:17 +0200

    spiders: fccid: Add request headers
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- certification_tracker/spiders/fccid.py
+++ certification_tracker/spiders/fccid.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
-from scrapy import Spider
+from scrapy import Spider, Request
 
 
 class FccidSpider(Spider):
     name = 'fccid'
     allowed_domains = ['fccid.io']
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Host': 'fccid.io',
+    }
     start_urls = ['https://fccid.io/2AFZZ/', 'https://fccid.io/2AIMR/']
 
     custom_settings = {
@@ -12,6 +20,10 @@ class FccidSpider(Spider):
             'certification_tracker.pipelines.fccid_pipeline.FccidPipeline': 400
         }
     }
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield Request(url, self.parse, headers=self.headers)
 
     def parse(self, response):
         table = response.xpath('//table[@class="table"]')[1]
